dd91c7f041a83197e01172bd28fe86c337cabcce
david19589 <davitnasyidashvili2006@gmail.com>
2024-06-24 14:25:32 +0400

    made 'data.ts' file in 'about' & 'home'components and moved objects

---
+++ src/components/about/data.ts
@@ -0,0 +1,85 @@
+import AvatarNikita from "/src/assets/avatar-nikita.jpg";
+import AvatarCristian from "/src/assets/avatar-cristian.jpg";
+import AvatarCruz from "/src/assets/avatar-cruz.jpg";
+import AvatarDrake from "/src/assets/avatar-drake.jpg";
+import AvatarGriffin from "/src/assets/avatar-griffin.jpg";
+import AvatarAden from "/src/assets/avatar-aden.jpg";
+import LogoVerge from "/src/assets/logo-the-verge.png";
+import LogoJakarta from "/src/assets/logo-jakarta-post.png";
+import LogoGuardian from "/src/assets/logo-the-guardian.png";
+import LogoRadar from "/src/assets/logo-tech-radar.png";
+import LogoGadgets from "/src/assets/logo-gadgets-now.png";
+
+export const infoData = [
+  {
+    src: AvatarNikita,
+    altText: "AvatarNikita",
+    name: "Nikita Marks",
+    position: "Founder & CEO",
+    paragraph:
+      "“It always amazes me how much talent there is in every corner of the globe.”",
+  },
+  {
+    src: AvatarCristian,
+    altText: "AvatarCristian",
+    name: "Cristian Duncan",
+    position: "Co-founder & COO",
+    paragraph:
+      "“Distributed teams required unique processes. You need to approach work in a new way.”",
+  },
+  {
+    src: AvatarCruz,
+    altText: "AvatarCruz",
+    name: "Cruz Hamer",
+    position: "Co-founder & CTO",
+    paragraph:
+      "“Technology is at the forefront of enabling distributed teams. That's where we come in.”",
+  },
+  {
+    src: AvatarDrake,
+    altText: "AvatarDrake",
+    name: "Drake Heaton",
+    position: "Business Development Lead",
+    paragraph:
+      "“Hiring similar people from similar backgrounds is a surefire way to stunt innovation.”",
+  },
+  {
+    src: AvatarGriffin,
+    altText: "AvatarGriffin",
+    name: "Griffin Wise",
+    position: "Lead Marketing",
+    paragraph:
+      "“Unique perspectives shape unique products, which is what you need to survive these days.”",
+  },
+  {
+    src: AvatarAden,
+    altText: "AvatarAden",
+    name: "Aden Allan",
+    position: "Head of Talent",
+    paragraph:
+      "“Empowered teams create truly amazing products. Set the north star and let them follow it.”",
+  },
+];
+
+export const logoData = [
+  {
+    src: LogoVerge,
+    altText: LogoVerge,
+  },
+  {
+    src: LogoJakarta,
+    altText: LogoJakarta,
+  },
+  {
+    src: LogoGuardian,
+    altText: LogoGuardian,
+  },
+  {
+    src: LogoRadar,
+    altText: LogoRadar,
+  },
+  {
+    src: LogoGadgets,
+    altText: LogoGadgets,
+  },
+];
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,62 +1,6 @@
-import ImgPerson from "/src/assets/icon-person.svg";
-import ImgCog from "/src/assets/icon-cog.svg";
-import ImgChart from "/src/assets/icon-chart.svg";
-import AvatarKady from "/src/assets/avatar-kady.jpg";
-import AvatarAiysha from "/src/assets/avatar-aiysha.jpg";
-import AvatarArthur from "/src/assets/avatar-arthur.jpg";
 import SuccessStories from "../success-stories";
 import { Link } from "react-router-dom";
-
-const infoData = [
-  {
-    src: ImgPerson,
-    altText: "ImgPerson",
-    heading: "Experienced Individuals",
-    paragraph:
-      "Our network is made up of highly experienced professionals who are passionate about what they do.",
-  },
-  {
-    src: ImgCog,
-    altText: "ImgCog",
-    heading: "Easy to Implement",
-    paragraph:
-      "Our processes have been refined over years of implementation meaning our teams always deliver.",
-  },
-  {
-    src: ImgChart,
-    altText: "ImgChart",
-    heading: "Enhanced Productivity",
-    paragraph:
-      "Our customized platform with in-built analytics helps you manage your distributed teams.",
-  },
-];
-
-const storiesData = [
-  {
-    src: AvatarKady,
-    altText: "Kady Baker",
-    paragraph:
-      "“The team perfectly fit the specialized skill set required. They focused on the most essential features helping us launch the platform eight months faster than planned.”",
-    author: "Kady Baker",
-    position: "Product Manager at Bookmark",
-  },
-  {
-    src: AvatarAiysha,
-    altText: "Aiysha Reese",
-    paragraph:
-      "“We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!”",
-    author: "Aiysha Reese",
-    position: "Founder of Manage",
-  },
-  {
-    src: AvatarArthur,
-    altText: "Arthur Clarke",
-    paragraph:
-      "“Amazing. Our team helped us build an app that delivered a new experience for hiring a physio. The launch was an instant success with 100k downloads in the first month.”",
-    author: "Arthur Clarke",
-    position: "Co-founder of MyPhysio",
-  },
-];
+import { infoData, storiesData } from "./data";
 
 function Home() {
   return (
